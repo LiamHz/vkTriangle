a5fc3d7ff4038f3174c532f72def051e44b3991f
LiamHz <liam.hinzman@gmail.com>
2020-07-14 15:48:31 -0400

    Queue family selection

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <optional>
 #include <cstdlib>
 #include <cstring>
 #include <vector>
@@ -68,6 +69,12 @@ private:
   VkInstance instance;
   VkDebugUtilsMessengerEXT debugMessenger;
   VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+  struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+    bool isComplete() {
+      return graphicsFamily.has_value();
+    }
+  };
 
   void initWindow() {
     glfwInit();
@@ -132,7 +139,37 @@ private:
       return 0;
     }
 
+    // Specify queue families that application requires
+    QueueFamilyIndices indices = findQueueFamilies(device);
+    if (!indices.isComplete()) {
+      return 0;
+    }
+
     return score;
+  }
+
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+
+    // Assign index to queue families that were found
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    int i = 0;
+    for (const auto& queueFamily : queueFamilies) {
+      if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+        indices.graphicsFamily = i;
+      }
+      if (indices.isComplete()) {
+        break;
+      }
+      i++;
+    }
+
+    return indices;
   }
 
   void mainLoop() {
